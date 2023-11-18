ad4664927cb8b213940732df8039a82a5da89a2e
CHINTAM-SRAVAN <143477892+CHINTAM-SRAVAN@users.noreply.github.com>
2023-11-18 19:16:20 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='home'>
-        <center><h1 style="color:blue;">RECIPE FINDER</h1>
-        <h3 style="color:orange;">The smartest way to find your recipe</h3></center>
+        <center><h1 style="color:blue;">RECIPE FINDER</h1></center>
+        <h3 style="color:orange;">The smartest way to find your recipe</h3>
     </div>
   )
 }
